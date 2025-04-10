d5c9910747444fad0295abdfc60527dfa2ac811f
saki <172128609+saki0506@users.noreply.github.com>
2025-04-10 11:28:49 +0900

    データ型の確認

---
+++ script.js
@@ -0,0 +1,9 @@
+const name ="John";
+const age = 30;
+const isMarried = false;
+const favoriteColors = ["blue","green","red"];
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof favoriteColors);
